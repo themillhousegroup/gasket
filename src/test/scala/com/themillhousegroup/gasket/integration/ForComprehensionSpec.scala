c1bd9e897ee5628e16c23e231bcb894d7e744b41
John Marshall <john@themillhousegroup.com>
2014-08-24 13:10:52 +1000

    New integration tests that also serve as example code

---
+++ src/test/scala/com/themillhousegroup/gasket/integration/ForComprehensionSpec.scala
@@ -0,0 +1,5 @@
+package com.themillhousegroup.gasket.integration
+
+class ForComprehensionSpec {
+
+}
---
+++ src/test/scala/com/themillhousegroup/gasket/test/GasketIntegrationSettings.scala
@@ -0,0 +1,5 @@
+package com.themillhousegroup.gasket.test
+
+trait GasketIntegrationSettings {
+
+}
