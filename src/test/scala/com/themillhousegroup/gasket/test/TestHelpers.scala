b7db959508b520f36835b550ce607f6b71314776
John Marshall <john.marshall2@auspost.com.au>
2014-11-06 12:37:54 +1100

    'Short wait' was a little too short on slow build nodes ...

--- src/test/scala/com/themillhousegroup/gasket/test/TestHelpers.scala
+++ src/test/scala/com/themillhousegroup/gasket/test/TestHelpers.scala
@@ -4,7 +4,7 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{ Future, Await }
 
 trait TestHelpers {
-  val shortWait = Duration(100L, "millis")
+  val shortWait = Duration(1000L, "millis")
 
   def waitFor[T](op: => Future[T]) = {
     Await.result(op, shortWait)
