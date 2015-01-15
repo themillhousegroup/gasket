38008204b34fb7650e74f26cf05a6fb2d4d92452
John Marshall <john.marshall2@auspost.com.au>
2015-01-16 10:46:16 +1100

    Access a row's cells by column name (e.g. 'C') for convenience. Tests

--- src/main/scala/com/themillhousegroup/gasket/Row.scala
+++ src/main/scala/com/themillhousegroup/gasket/Row.scala
@@ -3,6 +3,8 @@ package com.themillhousegroup.gasket
 /** Cells are what actually make up the Worksheet; Rows are basically a view onto them */
 case class Row(rowNumber: Int, cells: Seq[Cell]) extends Ordered[Row] {
 
+  lazy val matchChars = """([a-zA-Z]*)(\d*)""".r
+
   def compare(that: Row): Int = this.rowNumber - that.rowNumber
 
   /**
@@ -10,10 +12,27 @@ case class Row(rowNumber: Int, cells: Seq[Cell]) extends Ordered[Row] {
    * the `cells` Seq in this row might not be lined up
    * with the columns in the worksheet - i.e. cells(3) might not actually be
    * the fourth column in the sheet.
-   * This function provides a way to access the cells "by column"
+   * This function provides a way to access the cells by the "R1C1-style" 1-based column number
    * @param column
    */
   def cellAt(column: Int): Option[Cell] = {
     cells.find(_.colNumber == column)
+  }
+
+  /**
+   * Because the Google API omits blank cells,
+   * the `cells` Seq in this row might not be lined up
+   * with the columns in the worksheet - i.e. cells(3) might not actually be
+   * the fourth column in the sheet.
+   * This function provides a way to access the cells by the "A1-style" 1-based column name
+   * @param columnName
+   */
+  def cellAt(columnName: String): Option[Cell] = {
+    cells.find { cell =>
+      val columnTitle = cell.googleEntry.getTitle.getPlainText
+      columnTitle match {
+        case matchChars(colName, _) => colName == columnName
+      }
+    }
   }
 }
--- src/test/scala/com/themillhousegroup/gasket/RowSpec.scala
+++ src/test/scala/com/themillhousegroup/gasket/RowSpec.scala
@@ -4,26 +4,50 @@ import com.themillhousegroup.gasket.test.{ CellFeedTestFixtures, TestHelpers }
 import org.specs2.mutable.Specification
 import org.specs2.mock.Mockito
 import com.google.gdata.data.spreadsheet.CellEntry
+import com.google.gdata.data.TextConstruct
 
 class RowSpec extends Specification with Mockito with TestHelpers with CellFeedTestFixtures {
 
-  def mockCellAt(column: Int) = {
+  def mockCellAt(column: Int, columnName: String) = {
+    val mockTitle = mock[TextConstruct]
+    mockTitle.getPlainText returns s"${columnName}99"
+
+    val mockEntry = mock[CellEntry]
+    mockEntry.getTitle returns mockTitle
+
     val mockCell = mock[Cell]
+    mockCell.googleEntry returns mockEntry
     mockCell.colNumber returns column
     mockCell
   }
 
-  val mockCell1 = mockCellAt(3)
-  val mockCell2 = mockCellAt(6)
+  val mockCell1 = mockCellAt(3, "C")
+  val mockCell2 = mockCellAt(6, "F")
   val row = Row(1, Seq(mockCell1, mockCell2))
 
-  "cellAt should return None if no cell at desired column" in {
-    row.cellAt(0) must beNone
-    row.cellAt(1) must beNone
+  "cellAt(Int)" should {
+
+    "return None if no cell at desired column" in {
+      row.cellAt(0) must beNone
+      row.cellAt(1) must beNone
+    }
+
+    "return the cell at desired column" in {
+      row.cellAt(3) must beSome(mockCell1)
+      row.cellAt(6) must beSome(mockCell2)
+    }
   }
 
-  "cellAt should return the cell at desired column" in {
-    row.cellAt(3) must beSome(mockCell1)
-    row.cellAt(6) must beSome(mockCell2)
+  "cellAt(String)" should {
+
+    "return None if no cell at desired column" in {
+      row.cellAt("A") must beNone
+      row.cellAt("B") must beNone
+    }
+
+    "return the cell at desired column" in {
+      row.cellAt("C") must beSome(mockCell1)
+      row.cellAt("F") must beSome(mockCell2)
+    }
   }
 }
