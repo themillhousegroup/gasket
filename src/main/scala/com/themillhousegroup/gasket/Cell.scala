2fb4957489f11152b6e9556ef252a7f8c6f7ac7b
John Marshall <john.marshall2@auspost.com.au>
2015-01-15 11:11:21 +1100

    Support a primitive update mechanism

--- src/main/scala/com/themillhousegroup/gasket/Cell.scala
+++ src/main/scala/com/themillhousegroup/gasket/Cell.scala
@@ -2,6 +2,8 @@ package com.themillhousegroup.gasket
 
 import com.themillhousegroup.gasket.traits.ScalaEntry
 import com.google.gdata.data.spreadsheet.CellEntry
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class Cell(parent: Worksheet, val googleEntry: CellEntry) extends ScalaEntry[CellEntry] with Ordered[Cell] {
   lazy val value = googleEntry.getCell.getValue
@@ -13,4 +15,22 @@ case class Cell(parent: Worksheet, val googleEntry: CellEntry) extends ScalaEntr
   override def toString = s"[R${rowNumber}C${colNumber}]: $value"
 
   def compare(that: Cell): Int = this.colNumber - that.colNumber
+
+  /**
+   * THIS WILL NOT MUTATE THE CURRENT OBJECT!
+   *
+   * If newValue != value, pushes the change back to the source worksheet.
+   * @return a Future holding a Cell containing the updated value
+   */
+  def update(newValue: String): Future[Cell] = {
+    if (newValue == value) {
+      Future.successful(this)
+    } else {
+      Future {
+        val newEntry = new CellEntry(rowNumber, colNumber, newValue)
+        val fromRemote = newEntry.update // Blocks
+        Cell(parent, fromRemote)
+      }
+    }
+  }
 }
