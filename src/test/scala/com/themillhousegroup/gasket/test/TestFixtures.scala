75363100118099d2fb993928c05505bb3ed27f52
John Marshall <john.marshall2@auspost.com.au>
2015-01-28 16:49:05 +1100

    Refactored the test fixtures into a Scope trait to avoid mocking hell

--- src/test/scala/com/themillhousegroup/gasket/test/TestFixtures.scala
+++ src/test/scala/com/themillhousegroup/gasket/test/TestFixtures.scala
@@ -3,42 +3,13 @@ package com.themillhousegroup.gasket.test
 import com.google.gdata.client.spreadsheet.SpreadsheetService
 import com.google.gdata.data.spreadsheet._
 import java.net.URL
-import com.google.gdata.data.PlainTextConstruct
+import com.google.gdata.data.{ IFeed, PlainTextConstruct }
+import com.themillhousegroup.gasket.{ Account, Spreadsheet }
 import org.specs2.mock.Mockito
-import com.google.gdata.client.GoogleService.InvalidCredentialsException
-import com.themillhousegroup.gasket.Spreadsheet
-import scala.collection.JavaConverters._
+import org.specs2.specification.Scope
 
 trait TestFixtures {
   this: Mockito =>
-
-  val fakeUrl = new URL("http://localhost")
-
-  val mockService = mock[SpreadsheetService]
-
-  val s1 = mock[SpreadsheetEntry]
-  val s2 = mock[SpreadsheetEntry]
-  s1.getTitle returns new PlainTextConstruct("one")
-  s2.getTitle returns new PlainTextConstruct("two")
-
-  val mockSpreadsheetEntry = mock[SpreadsheetEntry]
-  mockSpreadsheetEntry.getWorksheetFeedUrl returns fakeUrl
-
-  val mockWorksheetFeed = mock[WorksheetFeed]
-  val w1 = mock[WorksheetEntry]
-  val w2 = mock[WorksheetEntry]
-  w1.getTitle returns new PlainTextConstruct("one")
-  w2.getTitle returns new PlainTextConstruct("two")
-
-  mockWorksheetFeed.getEntries returns com.google.common.collect.Lists.newArrayList(w1, w2)
-
-  val mockWorksheetEntry = mock[WorksheetEntry]
-  mockWorksheetEntry.getCellFeedUrl returns fakeUrl
-  mockWorksheetEntry.getListFeedUrl returns fakeUrl
-  mockWorksheetEntry.getTitle returns new PlainTextConstruct("mockWorksheetEntry")
-
-  val mockCellFeed = mock[CellFeed]
-  val mockEmptyCellFeed = mock[CellFeed]
 
   def mockCellEntry(row: Int, col: Int) = {
     val ce = mock[CellEntry]
@@ -51,26 +22,48 @@ trait TestFixtures {
     ce
   }
 
-  val c1 = mockCellEntry(1, 1)
-  val c2 = mockCellEntry(1, 2)
-  val c3 = mockCellEntry(2, 1)
-  val c4 = mockCellEntry(2, 2)
+  val fakeCellFeedUrl = new URL("http://localhost/cell")
+  val fakeBlockFeedUrl = "http://localhost/cell?min-row="
+  val fakeListFeedUrl = new URL("http://localhost/list")
+  val fakeWorksheetFeedUrl = new URL("http://localhost/worksheet")
 
-  mockCellFeed.getEntries returns com.google.common.collect.Lists.newArrayList(c1, c2, c3, c4)
-  mockWorksheetEntry.getRowCount returns 2
-  mockWorksheetEntry.getColCount returns 2
-}
+  abstract trait MockScope extends Scope {
 
-trait CellFeedTestFixtures extends TestFixtures {
-  this: Mockito =>
+    val s1 = mock[SpreadsheetEntry]
+    val s2 = mock[SpreadsheetEntry]
+    s1.getTitle returns new PlainTextConstruct("one")
+    s2.getTitle returns new PlainTextConstruct("two")
 
-  val mockSpreadsheet = mock[Spreadsheet]
+    val mockSpreadsheetEntry = mock[SpreadsheetEntry]
+    mockSpreadsheetEntry.getWorksheetFeedUrl returns fakeWorksheetFeedUrl
 
-  mockService.getFeed(any[URL], any[Class[CellFeed]]) returns mockCellFeed
-}
+    val mockWorksheetFeed = mock[WorksheetFeed]
+    val w1 = mock[WorksheetEntry]
+    val w2 = mock[WorksheetEntry]
+    w1.getTitle returns new PlainTextConstruct("one")
+    w2.getTitle returns new PlainTextConstruct("two")
 
-trait WorksheetFeedTestFixtures extends TestFixtures {
-  this: Mockito =>
+    mockWorksheetFeed.getEntries returns com.google.common.collect.Lists.newArrayList(w1, w2)
 
-  mockService.getFeed(any[URL], any[Class[WorksheetFeed]]) returns mockWorksheetFeed
+    val mockWorksheetEntry = mock[WorksheetEntry]
+    mockWorksheetEntry.getCellFeedUrl returns fakeCellFeedUrl
+    mockWorksheetEntry.getListFeedUrl returns fakeListFeedUrl
+    mockWorksheetEntry.getTitle returns new PlainTextConstruct("mockWorksheetEntry")
+
+    val mockCellFeed = mock[CellFeed]
+    val mockEmptyCellFeed = mock[CellFeed]
+
+    val c1 = mockCellEntry(1, 1)
+    val c2 = mockCellEntry(1, 2)
+    val c3 = mockCellEntry(2, 1)
+    val c4 = mockCellEntry(2, 2)
+
+    mockCellFeed.getEntries returns com.google.common.collect.Lists.newArrayList(c1, c2, c3, c4)
+    mockWorksheetEntry.getRowCount returns 2
+    mockWorksheetEntry.getColCount returns 2
+
+    val mockSpreadsheetFeed = mock[SpreadsheetFeed]
+    val mockSpreadsheet = mock[Spreadsheet]
+    val mockService: SpreadsheetService // Must be instantiated by subclasses
+  }
 }
