16c4a240432ac9cc1459dd07cf969c15f334f8aa
John Marshall <john@themillhousegroup.com>
2015-02-05 23:15:00 +1100

    A Block is compared based on the smallest row within it

--- src/main/scala/com/themillhousegroup/gasket/Block.scala
+++ src/main/scala/com/themillhousegroup/gasket/Block.scala
@@ -1,6 +1,8 @@
 package com.themillhousegroup.gasket
 
-case class Block(rowNumber: Int, cells: Seq[Cell]) extends Ordered[Block] {
+case class Block(cells: Seq[Cell]) extends Ordered[Block] {
 
-  def compare(that: Block): Int = this.rowNumber - that.rowNumber
+  lazy val minRow = cells.min.rowNumber
+
+  def compare(that: Block): Int = this.minRow - that.minRow
 }
