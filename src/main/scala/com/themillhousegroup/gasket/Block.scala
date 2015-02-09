5fb302a91ec3a64721841d435c345b2497cde25c
John Marshall <john@themillhousegroup.com>
2015-02-09 22:30:35 +1100

    More work on the Block abstraction

--- src/main/scala/com/themillhousegroup/gasket/Block.scala
+++ src/main/scala/com/themillhousegroup/gasket/Block.scala
@@ -1,8 +1,22 @@
 package com.themillhousegroup.gasket
 
-case class Block(cells: Seq[Cell]) extends Ordered[Block] {
+import scala.concurrent.Future
 
-  lazy val minRow = cells.min.rowNumber
+case class Block(parent:Worksheet, cells: Seq[Cell]) extends Ordered[Block] {
+
+  lazy val minRow = cells.head.rowNumber
+  lazy val minColumn = cells.head.colNumber
+
+  lazy val width =  cells.last.colNumber - minColumn
+  lazy val height =  cells.last.rowNumber - minRow
 
   def compare(that: Block): Int = this.minRow - that.minRow
+
+  /** A new value must be provided for each cell in the block,
+    * even if it's unchanged. The length of `newValues` must be
+    * the same as the original `cells` sequence.
+    */
+  def update(newValues:Seq[String]):Future[Block] = {
+    Future.successful(this)
+  }
 }
