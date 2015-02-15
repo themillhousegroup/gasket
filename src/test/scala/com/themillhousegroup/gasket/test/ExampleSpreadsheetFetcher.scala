7614ab9dd57a5359784fe1bd0cc517b8d4d911b7
John Marshall <john@themillhousegroup.com>
2015-02-15 14:18:22 +1100

    DRYed up the integration-test spreadsheet fetching

--- src/test/scala/com/themillhousegroup/gasket/test/ExampleSpreadsheetFetcher.scala
+++ src/test/scala/com/themillhousegroup/gasket/test/ExampleSpreadsheetFetcher.scala
@@ -1,36 +1,37 @@
 package com.themillhousegroup.gasket.test
 
-import com.themillhousegroup.gasket.{ Cell, Account, Row, Worksheet }
-import scala.concurrent.Await
+import com.themillhousegroup.gasket._
+import scala.concurrent.{ Future, Await }
+import com.themillhousegroup.gasket.Worksheet
+import com.themillhousegroup.gasket.Row
 
 object ExampleSpreadsheetFetcher extends TestHelpers {
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  //TODO: DRY these two up
+  def fetchSheet(username: String, password: String, sheetName: String): Future[Worksheet] = {
+    for {
+      acct <- Account(username, password)
+      ss <- acct.spreadsheets
+      ws <- ss("Example Spreadsheet").worksheets
+      sheet = ws(sheetName)
+    } yield (sheet)
+  }
+
+  def withSheet[T](username: String, password: String, sheetName: String)(f: (Worksheet) => Future[T]): (Worksheet, T) = {
+    Await.result({
+      for {
+        sheet <- fetchSheet(username, password, sheetName)
+        r <- f(sheet)
+      } yield (sheet, r)
+    }, shortWait)
+  }
 
   def fetchSheetAndRows(username: String, password: String, sheetName: String): (Worksheet, Seq[Row]) = {
-    val futureRows =
-      for {
-        acct <- Account(username, password)
-        ss <- acct.spreadsheets
-        ws <- ss("Example Spreadsheet").worksheets
-        sheet = ws(sheetName)
-        rows <- sheet.rows
-      } yield (sheet, rows)
-
-    Await.result(futureRows, shortWait)
+    withSheet(username, password, sheetName)(_.rows)
   }
 
   def fetchSheetAndCells(username: String, password: String, sheetName: String): (Worksheet, Seq[Cell]) = {
-    val futureCells =
-      for {
-        acct <- Account(username, password)
-        ss <- acct.spreadsheets
-        ws <- ss("Example Spreadsheet").worksheets
-        sheet = ws(sheetName)
-        cells <- sheet.cells
-      } yield (sheet, cells)
-
-    Await.result(futureCells, shortWait)
+    withSheet(username, password, sheetName)(_.cells)
   }
+
 }
