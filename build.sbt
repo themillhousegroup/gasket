d44d1e9512872787c410dd8e330f232a6678d63e
John Marshall <john@themillhousegroup.com>
2015-08-18 11:04:01 +1000

    OAuth 1.0 access is now disabled, reworking with OAuth 2.0. This will become Gasket 2.x

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "gasket"
 
-version := s"${sys.props.getOrElse("build.majorMinor", "1.1")}.${sys.props.getOrElse("build.version", "SNAPSHOT")}"
+version := s"${sys.props.getOrElse("build.majorMinor", "2.0")}.${sys.props.getOrElse("build.version", "SNAPSHOT")}"
 
 scalaVersion := "2.11.7"
 
@@ -10,6 +10,7 @@ organization := "com.themillhousegroup"
 
 libraryDependencies ++= Seq(
     "com.google.gdata"      %   "core"                    % "1.47.1",
+    "com.google.api-client"	%   "google-api-client"       % "1.20.0",
     "ch.qos.logback"        %   "logback-classic"         % "1.1.3",
     "org.mockito"           %   "mockito-all"             % "1.10.19",
     "org.specs2"            %%  "specs2"                  % "2.3.13"  % "test"
--- src/main/scala/com/themillhousegroup/gasket/Account.scala
+++ src/main/scala/com/themillhousegroup/gasket/Account.scala
@@ -1,14 +1,25 @@
 package com.themillhousegroup.gasket
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.gdata.client.spreadsheet.SpreadsheetService
 import java.net.URL
+import java.io.File
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed
 import com.themillhousegroup.gasket.traits.Timing
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.JavaConverters._
 
 object Account extends AccountBuilder {
   private[gasket] val SPREADSHEET_FEED_URL = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full")
+
+  // http://stackoverflow.com/questions/30483601/create-spreadsheet-using-google-spreadsheet-api-in-google-drive-in-java
+  //private[gasket] val CLIENT_ID = "1078815581589-ckv4dcn0f9lruaf4o678lu5edjk5kdma@developer.gserviceaccount.com" // notasecret
+
+  val scopesArray = List(
+    "https://spreadsheets.google.com/feeds",
+    "https://spreadsheets.google.com/feeds/spreadsheets/private/full",
+    "https://docs.google.com/feeds").asJava
 
   override lazy val service = new SpreadsheetService("gasket")
 
@@ -18,9 +29,14 @@ object Account extends AccountBuilder {
 protected[this] class AccountBuilder {
   lazy val service = new SpreadsheetService("gasket")
 
-  def apply(username: String, password: String): Future[Account] = Future {
+  def apply(clientId: String, p12: File): Future[Account] = Future {
+    val credential = new GoogleCredential.Builder().
+      setServiceAccountScopes(Account.scopesArray).
+      setServiceAccountId(clientId).
+      setServiceAccountPrivateKeyFromP12File(p12).
+      build()
 
-    service.setUserCredentials(username, password)
+    service.setOAuth2Credentials(credential)
     new Account(service)
   }
 }
