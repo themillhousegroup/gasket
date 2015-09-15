9064af11a689096dde9a8e1ae41785a08031999e
John Marshall <john@themillhousegroup.com>
2015-09-15 15:28:32 +1000

    Tweak test class to try and avoid test failures due to multiple parallel access to the Google account stuff, which seems thread-unsafe

--- src/test/scala/com/themillhousegroup/gasket/test/ExampleSpreadsheetFetcher.scala
+++ src/test/scala/com/themillhousegroup/gasket/test/ExampleSpreadsheetFetcher.scala
@@ -5,13 +5,23 @@ import scala.concurrent.{ Future, Await }
 import com.themillhousegroup.gasket.Worksheet
 import com.themillhousegroup.gasket.Row
 import java.io.File
+import scala.collection.mutable.Map
 
 object ExampleSpreadsheetFetcher extends TestHelpers {
   import scala.concurrent.ExecutionContext.Implicits.global
 
+	private val credentialAccountMap:scala.collection.mutable.Map[String, Future[Account]] = scala.collection.mutable.Map() 
+
+	private def lazyFetchAccount(clientId:String, p12File: File): Future[Account] = {
+ 		if (!credentialAccountMap.contains(clientId)) {
+			credentialAccountMap.put(clientId, Account(clientId, p12File))
+		}	
+		credentialAccountMap(clientId)
+	}
+
   def fetchSheet(clientId: String, p12File: File, sheetName: String): Future[Worksheet] = {
     for {
-      acct <- Account(clientId, p12File)
+      acct <- lazyFetchAccount(clientId, p12File)
       ss <- acct.spreadsheets
       ws <- ss("Example Spreadsheet").worksheets
       sheet = ws(sheetName)
@@ -34,5 +44,4 @@ object ExampleSpreadsheetFetcher extends TestHelpers {
   def fetchSheetAndCells(clientId: String, p12File: File, sheetName: String): (Worksheet, Seq[Cell]) = {
     withSheet(clientId, p12File, sheetName)(_.cells)
   }
-
 }
